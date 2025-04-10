891a00892c32172f6abe467028f0f730c518f133
SaeedAlian <aliansaeed82@gmail.com>
2025-04-10 20:28:16 +0330

    feat: update actions and resources enums

--- api/db/migrate/migrations/000001_initial.up.sql
+++ api/db/migrate/migrations/000001_initial.up.sql
@@ -1,25 +1,57 @@
 CREATE TYPE "actions" AS ENUM (
-    'can_add_product',
-    'can_update_product',
-    'can_delete_product',
-    'can_add_vendor',
-    'can_update_vendor',
-    'can_delete_vendor',
-    'can_ban_user',
-    'can_unban_user',
-    'can_add_product_tag',
-    'can_delete_product_tag',
-    'can_add_product_category',
-    'can_delete_product_category',
-    'can_delete_product_comment',
-    'can_add_role',
-    'can_delete_role',
-    'can_modify_role',
-    'can_add_permission_group',
-    'can_delete_permission_group',
-    'can_modify_permission_group'
+  'full_control',
+
+  'can_ban_user',
+  'can_unban_user',
+
+  'can_add_store',
+  'can_update_store'
+  'can_delete_store',
+
+  'can_create_order',
+  'can_change_order_status',
+  'can_verify_order',
+
+  'can_add_role',
+  'can_delete_role',
+  'can_modify_role',
+
+  'can_add_user_with_role',
+
+  'can_add_permission_group',
+  'can_delete_permission_group',
+  'can_modify_permission_group'
+
+  'can_assign_permission_group_to_role',
+  'can_remove_permission_group_from_role',
+
+  'can_add_product_category',
+  'can_modify_product_category',
+  'can_delete_product_category',
+
+  'can_add_product',
+  'can_update_product',
+  'can_delete_product',
+
+  'can_create_product_comment',
+  'can_update_product_comment',
+  'can_delete_product_comment'
 );
-CREATE TYPE "resources" AS ENUM ('products', 'orders', 'users', 'permissions');
+CREATE TYPE "resources" AS ENUM (
+  'full_access',
+
+  'roles_and_permissions', -- access to roles, permission groups, actions and resources permissions
+
+  'users_full_access', -- full user information even if the info is not public
+  'users_public_only', -- only public info of user
+
+  'wallet_transactions_full_access', -- full access to all wallet transactions for all users
+
+  'stores_full_access', -- full store information even if the info is not public
+  'stores_public_only', -- only public info of store
+
+  'orders_full_access' -- access to all orders with their transactions
+);
 CREATE TYPE "transaction_types" AS ENUM ('deposit', 'withdraw', 'purchase', 'sale');
 CREATE TYPE "transaction_status" AS ENUM ('pending', 'successful', 'failed');
 CREATE TYPE "order_status" AS ENUM ('pending_payment', 'pending_delivery', 'delivered', 'cancelled');
