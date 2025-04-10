a211a93743ec6fa4132a1dbcea35395a4c62d64b
SaeedAlian <aliansaeed82@gmail.com>
2025-04-10 14:23:54 +0330

    refactor: change vendor name to store

---
+++ api/db/migrate/migrations/000006_stores.up.sql
@@ -0,0 +1,40 @@
+CREATE TABLE stores (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  description VARCHAR(1023) NOT NULL,
+  verified BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
+
+ALTER TABLE addresses
+  ADD COLUMN store_id INTEGER REFERENCES stores(id) ON DELETE CASCADE;
+
+ALTER TABLE addresses
+  ALTER COLUMN user_id DROP NOT NULL;
+
+ALTER TABLE addresses
+  ADD CONSTRAINT address_has_owner CHECK (
+    (user_id IS NOT NULL AND store_id IS NULL) OR
+    (user_id IS NULL AND store_id IS NOT NULL)
+  );
+
+ALTER TABLE phonenumbers
+  ADD COLUMN store_id INTEGER REFERENCES stores(id) ON DELETE CASCADE;
+
+ALTER TABLE phonenumbers
+  ALTER COLUMN user_id DROP NOT NULL;
+
+ALTER TABLE phonenumbers
+  ADD CONSTRAINT phonenumber_has_owner CHECK (
+    (user_id IS NOT NULL AND store_id IS NULL) OR
+    (user_id IS NULL AND store_id IS NOT NULL)
+  );
+
+CREATE TABLE store_products (
+  store_id INTEGER NOT NULL REFERENCES stores(id) ON DELETE RESTRICT,
+  product_id INTEGER NOT NULL REFERENCES products(id) ON DELETE RESTRICT,
+  PRIMARY KEY (store_id, product_id)
+);
