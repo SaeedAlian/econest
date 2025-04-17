d9e6150fab84c2a2b3edf818c1cdf596f9fc840c
SaeedAlian <aliansaeed82@gmail.com>
2025-04-17 16:41:04 +0330

    fix: initial_data migration

--- api/db/migrate/migrations/000008_initial_data.down.sql
+++ api/db/migrate/migrations/000008_initial_data.down.sql
@@ -1,12 +1,12 @@
-DELETE FROM role_permission_groups WHERE
+DELETE FROM role_group_assignments WHERE
   role_id = (SELECT id FROM roles WHERE name = 'Super Admin') AND
   permission_group_id = (SELECT id FROM permission_groups WHERE name = 'Full Control');
 
-DELETE FROM action_permissions
+DELETE FROM group_action_permissions
   WHERE action = 'full_control' AND
   group_id = (SELECT id FROM permission_groups WHERE name = 'Full Control');
 
-DELETE FROM resource_permissions
+DELETE FROM group_resource_permissions
   WHERE resource = 'full_access' AND
   group_id = (SELECT id FROM permission_groups WHERE name = 'Full Control');
 
--- api/db/migrate/migrations/000008_initial_data.up.sql
+++ api/db/migrate/migrations/000008_initial_data.up.sql
@@ -4,15 +4,15 @@ INSERT INTO permission_groups
   (name, description) VALUES
   ('Full Control', 'Full access to all resources and actions');
 
-INSERT INTO resource_permissions
+INSERT INTO group_resource_permissions
   (resource, group_id) VALUES
   ('full_access', (SELECT id FROM permission_groups WHERE name = 'Full Control'));
 
-INSERT INTO action_permissions
+INSERT INTO group_action_permissions
   (action, group_id) VALUES
   ('full_control', (SELECT id FROM permission_groups WHERE name = 'Full Control'));
 
-INSERT INTO role_permission_groups 
+INSERT INTO role_group_assignments 
   (role_id, permission_group_id) VALUES
   (
     (SELECT id FROM roles WHERE name = 'Super Admin'),
