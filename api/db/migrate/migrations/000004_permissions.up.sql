7810bc629f31fcb55f8a9b53d57c0950e5224793
SaeedAlian <aliansaeed82@gmail.com>
2025-05-03 15:04:23 +0330

    fix: remove primary key from group_resource_permissions & group_action_permissions   -Added a composite key from action/resource and group_id instead

--- api/db/migrate/migrations/000004_permissions.up.sql
+++ api/db/migrate/migrations/000004_permissions.up.sql
@@ -12,22 +12,22 @@ CREATE TABLE role_group_assignments (
 );
 
 CREATE TABLE group_resource_permissions (
-  id SERIAL PRIMARY KEY,
-  resource VARCHAR(63) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-  group_id INTEGER NOT NULL REFERENCES permission_groups(id) ON DELETE CASCADE
+  resource VARCHAR(63) NOT NULL,
+  group_id INTEGER NOT NULL REFERENCES permission_groups(id) ON DELETE CASCADE,
+  PRIMARY KEY (resource, group_id)
 );
 
 ALTER TABLE group_resource_permissions
   ALTER COLUMN resource TYPE resources USING resource::resources;
 
 CREATE TABLE group_action_permissions (
-  id SERIAL PRIMARY KEY,
-  action VARCHAR(63) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-  group_id INTEGER NOT NULL REFERENCES permission_groups(id) ON DELETE CASCADE
+  action VARCHAR(63) NOT NULL,
+  group_id INTEGER NOT NULL REFERENCES permission_groups(id) ON DELETE CASCADE,
+  PRIMARY KEY (action, group_id)
 );
 
 ALTER TABLE group_action_permissions
