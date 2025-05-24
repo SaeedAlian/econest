ffeda4367d0a501512f21c8eaa682217d6796ac1
SaeedAlian <aliansaeed82@gmail.com>
2025-05-24 16:54:59 +0330

    feat: setup address & phone number methods in user service

--- api/services/user/routes.go
+++ api/services/user/routes.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -31,16 +32,29 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 
 	authRouter := router.Methods("POST").Subrouter()
 	authRouter.HandleFunc("/login", h.login)
-	authRouter.HandleFunc("/refresh", h.refresh)
+	authRouter.HandleFunc("/refresh", h.refresh).Methods("POST")
 
 	logoutRouter := router.Methods("POST").Subrouter()
 	logoutRouter.HandleFunc("/logout", h.logout)
 	logoutRouter.Use(h.authHandler.WithJWTAuth(h.db))
 	logoutRouter.Use(h.authHandler.WithCSRFToken())
 
-	withAuthRouter := router.NewRoute().Subrouter()
+	withAuthRouter := router.Methods("GET", "POST", "PATCH", "DELETE").Subrouter()
 	withAuthRouter.HandleFunc("/me", h.getMe)
 	withAuthRouter.Use(h.authHandler.WithJWTAuth(h.db))
+	withAuthRouter.Use(h.authHandler.WithCSRFToken())
+
+	addressHandlerRouter := withAuthRouter.PathPrefix("/address").Subrouter()
+	addressHandlerRouter.HandleFunc("/", h.createAddress).Methods("POST")
+	addressHandlerRouter.HandleFunc("/", h.getMyAddresses).Methods("GET")
+	addressHandlerRouter.HandleFunc("/{addrId}", h.updateAddress).Methods("PATCH")
+	addressHandlerRouter.HandleFunc("/{addrId}", h.deleteAddress).Methods("DELETE")
+
+	phoneNumberHandlerRouter := withAuthRouter.PathPrefix("/phonenumber").Subrouter()
+	phoneNumberHandlerRouter.HandleFunc("/", h.createPhoneNumber).Methods("POST")
+	phoneNumberHandlerRouter.HandleFunc("/", h.getMyPhoneNumbers).Methods("GET")
+	phoneNumberHandlerRouter.HandleFunc("/{phoneId}", h.updatePhoneNumber).Methods("PATCH")
+	phoneNumberHandlerRouter.HandleFunc("/{phoneId}", h.deletePhoneNumber).Methods("DELETE")
 }
 
 func (h *Handler) register(roleName string) func(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +71,7 @@ func (h *Handler) register(roleName string) func(w http.ResponseWriter, r *http.
 			utils.WriteErrorInResponse(
 				w,
 				http.StatusBadRequest,
-				types.ErrInvalidPayload(errors),
+				types.ErrInvalidPayload(errors[0]),
 			)
 			return
 		}
@@ -123,7 +137,7 @@ func (h *Handler) login(w http.ResponseWriter, r *http.Request) {
 
 	if err := utils.Validator.Struct(payload); err != nil {
 		errors := err.(validator.ValidationErrors)
-		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors))
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors[0]))
 		return
 	}
 
@@ -389,4 +403,401 @@ func (h *Handler) getMe(w http.ResponseWriter, r *http.Request) {
 	})
 
 	utils.WriteJSONInResponse(w, http.StatusOK, userRes, nil)
+}
+
+func (h *Handler) createAddress(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userId := ctx.Value("userId")
+
+	if userId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	var payload types.CreateUserAddressPayload
+	if err := utils.ParseJSONFromRequest(r, &payload); err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidAddressPayload)
+		return
+	}
+
+	if err := utils.Validator.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors[0]))
+		return
+	}
+
+	addrId, err := h.db.CreateUserAddress(types.CreateUserAddressPayload{
+		City:    payload.City,
+		State:   payload.State,
+		Street:  payload.Street,
+		Zipcode: payload.Zipcode,
+		Details: payload.Details,
+		UserId:  userId.(int),
+	})
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrCreateAddress)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusCreated, map[string]int{"addressId": addrId}, nil)
+}
+
+func (h *Handler) createPhoneNumber(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userId := ctx.Value("userId")
+
+	if userId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	var payload types.CreateUserPhoneNumberPayload
+	if err := utils.ParseJSONFromRequest(r, &payload); err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPhoneNumberPayload)
+		return
+	}
+
+	if err := utils.Validator.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors[0]))
+		return
+	}
+
+	phoneId, err := h.db.CreateUserPhoneNumber(types.CreateUserPhoneNumberPayload{
+		CountryCode: payload.CountryCode,
+		Number:      payload.Number,
+		UserId:      userId.(int),
+	})
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrCreatePhoneNumber)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusCreated, map[string]int{"phoneNumberId": phoneId}, nil)
+}
+
+func (h *Handler) getMyAddresses(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userId := ctx.Value("userId")
+
+	if userId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	visibilityStatusQuery := r.URL.Query().Get("visibility")
+	var visibilityStatus *types.SettingVisibilityStatus = nil
+
+	if visibilityStatusQuery != "" {
+		visibilityStatus = utils.Ptr(types.SettingVisibilityStatus(visibilityStatusQuery))
+		if !visibilityStatus.IsValid() {
+			utils.WriteErrorInResponse(
+				w,
+				http.StatusBadRequest,
+				types.ErrInvalidVisibilityStatusOption,
+			)
+			return
+		}
+	}
+
+	query := types.UserAddressSearchQuery{
+		VisibilityStatus: visibilityStatus,
+	}
+
+	addresses, err := h.db.GetUserAddresses(userId.(int), query)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusInternalServerError, types.ErrInternalServer)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, addresses, nil)
+}
+
+func (h *Handler) getMyPhoneNumbers(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	userId := ctx.Value("userId")
+
+	if userId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	visibilityStatusQuery := r.URL.Query().Get("visibility")
+	var visibilityStatus *types.SettingVisibilityStatus = nil
+
+	verificationStatusQuery := r.URL.Query().Get("verified")
+	var verificationStatus *types.CredentialVerificationStatus = nil
+
+	if visibilityStatusQuery != "" {
+		visibilityStatus = utils.Ptr(types.SettingVisibilityStatus(visibilityStatusQuery))
+		if !visibilityStatus.IsValid() {
+			utils.WriteErrorInResponse(
+				w,
+				http.StatusBadRequest,
+				types.ErrInvalidVisibilityStatusOption,
+			)
+			return
+		}
+	}
+
+	if verificationStatusQuery != "" {
+		verificationStatus = utils.Ptr(types.CredentialVerificationStatus(verificationStatusQuery))
+		if !verificationStatus.IsValid() {
+			utils.WriteErrorInResponse(
+				w,
+				http.StatusBadRequest,
+				types.ErrInvalidVerificationStatusOption,
+			)
+			return
+		}
+	}
+
+	query := types.UserPhoneNumberSearchQuery{
+		VerificationStatus: verificationStatus,
+		VisibilityStatus:   visibilityStatus,
+	}
+
+	phones, err := h.db.GetUserPhoneNumbers(userId.(int), query)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusInternalServerError, types.ErrInternalServer)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, phones, nil)
+}
+
+func (h *Handler) updateAddress(w http.ResponseWriter, r *http.Request) {
+	addrIdParam := mux.Vars(r)["addrId"]
+
+	addrId, err := strconv.Atoi(addrIdParam)
+	if err != nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusBadRequest,
+			types.ErrInvalidAddressId,
+		)
+		return
+	}
+
+	ctx := r.Context()
+
+	cUserId := ctx.Value("userId")
+
+	if cUserId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	userId := cUserId.(int)
+
+	var payload types.UpdateUserAddressPayload
+	if err := utils.ParseJSONFromRequest(r, &payload); err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidAddressPayload)
+		return
+	}
+
+	if err := utils.Validator.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors[0]))
+		return
+	}
+
+	userAddr, err := h.db.GetUserAddressById(addrId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusNotFound, types.ErrUserAddressNotFound)
+		return
+	}
+
+	if userAddr.UserId != userId {
+		utils.WriteErrorInResponse(w, http.StatusForbidden, types.ErrCannotAccessAddress)
+		return
+	}
+
+	err = h.db.UpdateUserAddress(addrId, userId, payload)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrUpdateAddress)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, nil, nil)
+}
+
+func (h *Handler) updatePhoneNumber(w http.ResponseWriter, r *http.Request) {
+	phoneIdParam := mux.Vars(r)["phoneId"]
+
+	phoneId, err := strconv.Atoi(phoneIdParam)
+	if err != nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusBadRequest,
+			types.ErrInvalidPhoneNumberId,
+		)
+		return
+	}
+
+	ctx := r.Context()
+
+	cUserId := ctx.Value("userId")
+
+	if cUserId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	userId := cUserId.(int)
+
+	var payload types.UpdateUserPhoneNumberPayload
+	if err := utils.ParseJSONFromRequest(r, &payload); err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPhoneNumberPayload)
+		return
+	}
+
+	if err := utils.Validator.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrInvalidPayload(errors[0]))
+		return
+	}
+
+	userPhone, err := h.db.GetUserPhoneNumberById(phoneId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusNotFound, types.ErrUserPhoneNumberNotFound)
+		return
+	}
+
+	if userPhone.UserId != userId {
+		utils.WriteErrorInResponse(w, http.StatusForbidden, types.ErrCannotAccessPhoneNumber)
+		return
+	}
+
+	err = h.db.UpdateUserPhoneNumber(phoneId, userId, payload)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrUpdatePhoneNumber)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, nil, nil)
+}
+
+func (h *Handler) deleteAddress(w http.ResponseWriter, r *http.Request) {
+	addrIdParam := mux.Vars(r)["addrId"]
+
+	addrId, err := strconv.Atoi(addrIdParam)
+	if err != nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusBadRequest,
+			types.ErrInvalidAddressId,
+		)
+		return
+	}
+
+	ctx := r.Context()
+
+	cUserId := ctx.Value("userId")
+
+	if cUserId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	userId := cUserId.(int)
+
+	userAddr, err := h.db.GetUserAddressById(addrId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusNotFound, types.ErrUserAddressNotFound)
+		return
+	}
+
+	if userAddr.UserId != userId {
+		utils.WriteErrorInResponse(w, http.StatusForbidden, types.ErrCannotAccessAddress)
+		return
+	}
+
+	err = h.db.DeleteUserAddress(addrId, userId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrDeleteAddress)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, nil, nil)
+}
+
+func (h *Handler) deletePhoneNumber(w http.ResponseWriter, r *http.Request) {
+	phoneIdParam := mux.Vars(r)["phoneId"]
+
+	phoneId, err := strconv.Atoi(phoneIdParam)
+	if err != nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusBadRequest,
+			types.ErrInvalidPhoneNumberId,
+		)
+		return
+	}
+
+	ctx := r.Context()
+
+	cUserId := ctx.Value("userId")
+
+	if cUserId == nil {
+		utils.WriteErrorInResponse(
+			w,
+			http.StatusUnauthorized,
+			types.ErrAuthenticationCredentialsNotFound,
+		)
+		return
+	}
+
+	userId := cUserId.(int)
+
+	userPhone, err := h.db.GetUserPhoneNumberById(phoneId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusNotFound, types.ErrUserPhoneNumberNotFound)
+		return
+	}
+
+	if userPhone.UserId != userId {
+		utils.WriteErrorInResponse(w, http.StatusForbidden, types.ErrCannotAccessPhoneNumber)
+		return
+	}
+
+	err = h.db.DeleteUserPhoneNumber(phoneId, userId)
+	if err != nil {
+		utils.WriteErrorInResponse(w, http.StatusBadRequest, types.ErrDeletePhoneNumber)
+		return
+	}
+
+	utils.WriteJSONInResponse(w, http.StatusOK, nil, nil)
 }
