53048823e665ded49dbc6ac50434b2002823b222
SaeedAlian <aliansaeed82@gmail.com>
2025-09-11 01:50:33 +0330

    feat: add product searchbar in home navbar

--- src/components/home/navbar.tsx
+++ src/components/home/navbar.tsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 import { cn } from "@/lib/utils";
+
 import { useIsMobile } from "@/hooks/use-mobile";
+
 import { Button } from "@/components/ui/button";
+import { ProductSearchBar } from "@/components/ui/product-searchbar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+
 import logo from "@/assets/logo-dark.svg";
 
 export type NavbarLink = {
@@ -24,14 +28,15 @@ export type NavbarProps = {
 };
 
 function Navbar({ links }: NavbarProps) {
+  const nav = useNavigate();
   const isMobile = useIsMobile();
 
   return (
-    <nav className="flex items-center flex-row gap-2 w-full justify-between px-5 py-2">
+    <nav className="flex items-center flex-row gap-2 w-full px-5 py-2 sticky top-0 bg-background/80 backdrop-blur z-20">
       <Link
         to="/"
         className={cn(
-          "flex items-center justify-center m-[-20px]",
+          "flex items-center justify-center m-[-20px] mr-auto",
           isMobile ? "m-[-25px]" : "",
         )}
       >
@@ -41,10 +46,17 @@ function Navbar({ links }: NavbarProps) {
           className={cn("w-24 h-24", isMobile ? "w-20 h-20" : "")}
         />
       </Link>
+      <div className="w-full max-w-[390px] mr-7 max-[1120px]:hidden">
+        <ProductSearchBar
+          onSelect={(p) => {
+            nav(`/product/${p.id}`);
+          }}
+        />
+      </div>
       {isMobile ? (
         <>
           <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger className="ml-auto">
               <GiHamburgerMenu />
             </DropdownMenuTrigger>
             <DropdownMenuContent>
